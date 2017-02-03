b84097a664ff8cd5bb41b086303615833daaa964
QiZW <qizewei@vip.qq.com>
2017-02-03 13:29:34 +0800

    添加混淆，安装包体积缩小一倍。

--- app/src/main/java/com/filemanager/MyApplication.java
+++ app/src/main/java/com/filemanager/MyApplication.java
@@ -4,7 +4,7 @@ import android.app.Application;
 
 import com.umeng.analytics.MobclickAgent;
 
-import cn.bmob.v3.Bmob;
+
 
 /**
  * Created by 齐泽威 on 2017/2/1.
@@ -14,7 +14,7 @@ public class MyApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        Bmob.initialize(this, "9cefd91191d36d9023985dc24c860b39");
+        cn.bmob.v3.statistics.AppStat.i("9cefd91191d36d9023985dc24c860b39", "MoRen");
 
         MobclickAgent.UMAnalyticsConfig config = new MobclickAgent.UMAnalyticsConfig(this, "9cefd91191d36d9023985dc24c860b39", "MoRen");
         MobclickAgent.startWithConfigure(config);
