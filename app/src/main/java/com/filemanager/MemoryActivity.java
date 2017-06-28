3ad890a92532ff445c5725f411f59dded00931ef
QzwCompany <qizewei@vip.qq.com>
2017-06-28 19:26:33 +0800

    修改一些Warning

--- app/src/main/java/com/filemanager/MemoryActivity.java
+++ app/src/main/java/com/filemanager/MemoryActivity.java
@@ -16,7 +16,6 @@ public class MemoryActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_memory);
         initView();
-
     }
 
     private void initView() {
@@ -33,41 +32,41 @@ public class MemoryActivity extends AppCompatActivity {
         String manufacturer = DeviceUtils.getManufacturer();
         boolean root = DeviceUtils.isDeviceRoot();
         int version = DeviceUtils.getSDKVersion();
-        String imei = PhoneUtils.getIMEI(this);
+        String IMEI = PhoneUtils.getIMEI(this);
 
-        TextView DetialModel = (TextView) findViewById(R.id.detial_model);
-        DetialModel.setText(model);
-        TextView DetialFactory = (TextView) findViewById(R.id.detial_factory);
-        DetialFactory.setText(manufacturer);
-        TextView DetialImei = (TextView) findViewById(R.id.detial_imei);
-        DetialImei.setText(imei);
+        TextView DetailModelModel = (TextView) findViewById(R.id.detial_model);
+        DetailModelModel.setText(model);
+        TextView DetailFactory = (TextView) findViewById(R.id.detial_factory);
+        DetailFactory.setText(manufacturer);
+        TextView DetailIMEI = (TextView) findViewById(R.id.detial_imei);
+        DetailIMEI.setText(IMEI);
 
-        TextView DetialRoot = (TextView) findViewById(R.id.detial_root);
+        TextView DetailRoot = (TextView) findViewById(R.id.detial_root);
         if (root) {
-            DetialRoot.setText("你的设备已Root");
+            DetailRoot.setText("你的设备已Root");
         } else
-            DetialRoot.setText("您的设备未Root");
-        TextView DetialVersion = (TextView) findViewById(R.id.detial_version);
+            DetailRoot.setText("您的设备未Root");
+        TextView DetailVersion = (TextView) findViewById(R.id.detial_version);
         if (version == 23) {
-            DetialVersion.setText("6.0.0");
+            DetailVersion.setText("6.0.0");
         } else if (version == 22) {
-            DetialVersion.setText("5.1.0");
+            DetailVersion.setText("5.1.0");
         } else if (version == 21) {
-            DetialVersion.setText("5.0.0");
+            DetailVersion.setText("5.0.0");
         } else if (version == 20) {
-            DetialVersion.setText("4.4.0 W");
+            DetailVersion.setText("4.4.0 W");
         } else if (version == 19) {
-            DetialVersion.setText("4.4.0");
+            DetailVersion.setText("4.4.0");
         } else if (version == 18) {
-            DetialVersion.setText("4.3.0");
+            DetailVersion.setText("4.3.0");
         } else if (version == 17) {
-            DetialVersion.setText("4.2.0");
+            DetailVersion.setText("4.2.0");
         } else if (version == 16) {
-            DetialVersion.setText("4.1.0");
+            DetailVersion.setText("4.1.0");
         } else if (version == 15) {
-            DetialVersion.setText("4.0.0");
+            DetailVersion.setText("4.0.0");
         } else if (version <= 14) {
-            DetialVersion.setText("恭喜，你是地球上Android版本最低的3%的人之一。");
+            DetailVersion.setText("恭喜，你是地球上Android版本最低的3%的人之一。");
         }
     }
 
